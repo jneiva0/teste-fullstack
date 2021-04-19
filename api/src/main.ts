ca3b8e1dcbdc4c218b491698ff0376fa005bc83f
Joao Neiva <jneiva42@gmail.com>
2021-04-19 12:54:43 -0300

    configure cors to accept connections from the default port when serving the frontend

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'http://localhost:5000'],
   })
 
   await app.listen(4000)
