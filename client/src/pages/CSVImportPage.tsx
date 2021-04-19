934da1749f56a38041fa384caf1f8666a1f3b875
Joao Neiva <jneiva42@gmail.com>
2021-04-19 09:47:29 -0300

    create components to parse and show the data

--- client/src/pages/CSVImportPage.tsx
+++ client/src/pages/CSVImportPage.tsx
@@ -1,21 +1,99 @@
-import { Box, Container, Text } from '@chakra-ui/react'
-import React, { useCallback } from 'react'
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Container,
+  Divider,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
+import { parse, ParseResult } from 'papaparse'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
-
-export const CSVImportPage = () => {
-  const onUpload = (file: File) => {
-    console.log(file)
-  }
-  return (
-    <Container pt={16}>
-      <FileUploadInput onUpload={onUpload} />
-    </Container>
-  )
-}
 
 // Vou deixar os componentes usados aqui nesse arquivo para não misturar com os da
 // atividade 1. No geral prefiro deixar um componente por arquivo e nomear o arquivo com o nome do componente
 
+// Estou usando uma biblioteca para fazer o parse do arquivo, não sei se o objetivo era
+// implementar do zero ou apenas ter a feature funcionando. Por motivos de tempo fui na alternativa mais rápida
+// Vi alguns métodos para ler o CSV, se eu fosse implementar eu tentaria algo usando Regex
+// Acho que para lidar com todos os casos possíveis a alternativa mais promissora seria usando State Machines
+
+// Pela falta de tempo estou presumindo algumas coisas.
+// Estou considerando que o CSV esteja estruturado com a primeira linha como cabeçalho
+// a biblioteca também já está fazendo a conversão de números e boolean
+
+// Está faltando lidar com o tipo Data
+
+export const CSVImportPage = () => {
+  const [result, setResult] = useState<ParseResult<unknown>>()
+
+  const onUpload = (file: File) => {
+    parse(file, {
+      complete: res => {
+        return setResult(res)
+      },
+      header: true,
+      dynamicTyping: true,
+    })
+  }
+  return (
+    <Container maxW='container.lg' pt={16}>
+      <FileUploadInput onUpload={onUpload} />
+      <Divider my={3} />
+      {result && <ResultTable result={result} />}
+    </Container>
+  )
+}
+
+// A Tabela funciona bem caso as linhas tenham o mesmo número de campos
+// Linhas incompletas são ignoradas, está longe do ideal mas ao menos é mostrado
+// quais linhas tiveram problema.
+const ResultTable = ({ result }: { result: ParseResult<unknown> }) => {
+  return (
+    <>
+      {result.errors.map(error => (
+        <Alert key={error.row} status='error'>
+          <AlertIcon />
+          <AlertTitle mr={2}>Linha {error.row}</AlertTitle>
+          <AlertDescription>{error.message}</AlertDescription>
+        </Alert>
+      ))}
+      <Table mt={8}>
+        <Thead>
+          <Tr>
+            {result.meta.fields?.map(field => (
+              <Th key={field}>{field}</Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {/* Estou usando o indice como key já que não consigo prever o formato dos dados */}
+          {result.data.map((row, i) => (
+            <Tr key={i}>
+              {Object.values(row as Record<string, string | number>).map(
+                (cell, i) => (
+                  <Td key={i}>{cell}</Td>
+                )
+              )}
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
+  )
+}
+
+// Estou usando uma biblioteca chamada react-dropzone para carregar o arquivo por questão de praticidade
+// Como é algo simples, em um projeto real essa função poderia ser implementada do zero
+// para evitar uma dependencia a mais no projeto
 const FileUploadInput = ({ onUpload }: { onUpload: (file: File) => void }) => {
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
